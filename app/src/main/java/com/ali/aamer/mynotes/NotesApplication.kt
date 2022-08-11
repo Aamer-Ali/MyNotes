76f55b2cd073be83475b982f47326eacb5cc00eb
Aamer Ashar Ali Sayyed <sayyed.aamer.ali@gmail.com>
2022-08-11 16:08:03 +0530

    Resources and xml layouts are added.Theme is changed to material3. All the fragments are added and setup with view binding. Navigation also added from jetpack. The Clean structure also created with the two features ( authentication and notes )

---
+++ app/src/main/java/com/ali/aamer/mynotes/NotesApplication.kt
@@ -0,0 +1,7 @@
+package com.ali.aamer.mynotes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NotesApplication : Application()
---
+++ app/src/main/java/com/ali/aamer/mynotes/features/feature_notes/display/fragments/AddEditNoteFragment.kt
@@ -0,0 +1,35 @@
+package com.ali.aamer.mynotes.features.feature_notes.display.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ali.aamer.mynotes.databinding.FragmentAddEditNoteBinding
+
+
+
+class AddEditNoteFragment : Fragment() {
+
+    private var _binding: FragmentAddEditNoteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentAddEditNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
